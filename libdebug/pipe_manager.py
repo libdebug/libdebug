09475240eb11978ef7cf76aee8fe82170d34ce73
gabriele180698 <gabriele18.06.98@gmail.com>
2023-11-05 23:03:47 +0100

    feat: add recvline feature

--- libdebug/pipe_manager.py
+++ libdebug/pipe_manager.py
@@ -13,12 +13,12 @@ class PipeManager:
     timeout_default: int = 2
 
     def __init__(self, stdin_write: int, stdout_read: int, stderr_read: int):
-        """Constructor for PipeManager class
+        """Constructor for PipeManager class.
         
         Args:
-            stdin_write (int): file descriptor for stdin write
-            stdout_read (int): file descriptor for stdout read
-            stderr_read (int): file descriptor for stderr read
+            stdin_write (int): file descriptor for stdin write.
+            stdout_read (int): file descriptor for stdout read.
+            stderr_read (int): file descriptor for stderr read.
         """
         self.stdin_write: int = stdin_write
         self.stdout_read: int = stdout_read
@@ -33,11 +33,11 @@ class PipeManager:
             timeout (int, optional): timeout in seconds. Defaults to timeout_default.
 
         Returns:
-            bytes: received bytes from the child process stdout
+            bytes: received bytes from the child process stdout.
 
         Raises:
-            ValueError: numb is negative
-            PipeFail: no stdout pipe of the child process
+            ValueError: numb is negative.
+            PipeFail: no stdout pipe of the child process.
         """
 
         if not self.stdout_read:
@@ -81,4 +81,79 @@ class PipeManager:
                 data = os.read(self.stdout_read, 4096)
                 data_buffer += data
                     
+        return data_buffer
+    
+
+    def _recvline(self, keepends: bool=False, timeout: int=timeout_default) -> bytes:
+        """Receives a line from the child process stdout.
+        
+        Args:
+            keepends (bool, optional): keep the line ending. Defaults to False.
+            timeout (int, optional): timeout in seconds. Defaults to timeout_default.
+
+        Returns:
+            bytes: received line from the child process stdout.
+
+        Raises:
+            TimeoutError: timeout reached.
+        """
+
+        # Buffer for the received data
+        data_buffer = b''
+
+        # Setting the alarm
+        end_time = time.time() + timeout
+        while True:
+            if end_time is not None and time.time() > end_time:
+                # Timeout reached
+                raise TimeoutError("Timeout reached")
+
+            # Adjust the timeout for select to the remaining time
+            remaining_time = None if end_time is None else max(0, end_time - time.time())
+            
+            data = self.recv(1, remaining_time)
+            
+            if data != b'\n':
+                data_buffer += data
+            else:
+                # End of line reached
+                if keepends:
+                    data_buffer += data
+                break
+            
+
+        return data_buffer
+
+
+    def recvline(self, numlines: int=1, keepends: bool=False, timeout: int=timeout_default) -> bytes:
+        """Receives numlines lines from the child process stdout.
+        
+        Args:
+            numlines (int, optional): number of lines to receive. Defaults to 1.
+            keepends (bool, optional): keep the line ending. Defaults to False.
+            timeout (int, optional): timeout in seconds. Defaults to timeout_default.
+
+        Returns:
+            bytes: received lines from the child process stdout.
+
+        Raises:
+            ValueError: numlines is negative.
+            TimeoutError: timeout reached.
+        """
+
+        if numlines <= 0:
+            raise ValueError("The number of lines to receive must be positive")
+
+        # Buffer for the received data
+        data_buffer = b''
+
+        # Setting the alarm
+        end_time = time.time() + timeout
+
+        for _ in range(numlines):
+            # Adjust the timeout for select to the remaining time
+            remaining_time = None if end_time is None else max(0, end_time - time.time())
+
+            data_buffer += self._recvline(keepends, remaining_time)
+
         return data_buffer
