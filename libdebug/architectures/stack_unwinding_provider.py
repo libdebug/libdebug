4b87f94b029a7ed556e820fb275a48176763e45b
io-no <gabriele18.06.98@gmail.com>
2023-11-29 01:34:12 +0100

    fix: add provider

---
+++ libdebug/architectures/stack_unwinding_provider.py
@@ -0,0 +1,28 @@
+#
+# This file is part of libdebug Python library (https://github.com/io-no/libdebug).
+# Copyright (c) 2023 Roberto Gabriele Digregorio.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from libdebug.architectures.amd64.amd64_stack_unwinding import (
+    Amd64StackUnwinding,
+)
+
+def stack_unwinding_provider(architecture: str = "amd64") -> Amd64StackUnwinding:
+    """Returns an instance of the stack unwinding provider to be used by the `Debugger` class."""
+    match architecture:
+        case "amd64":
+            return Amd64StackUnwinding()
+        case _:
+            raise NotImplementedError(f"Architecture {architecture} not available.")
