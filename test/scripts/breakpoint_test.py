0acf8f76dbe29ca088ac10cd71e3cda6a5419228
MrIndeciso <robertobertolini37@gmail.com>
2024-03-17 13:08:00 +0100

    test: add test for breakpoint enable and disable

--- test/scripts/breakpoint_test.py
+++ test/scripts/breakpoint_test.py
@@ -64,6 +64,196 @@ class BreakpointTest(unittest.TestCase):
 
             d.cont()
 
+        self.assertEqual(bp2.hit_count, 10)
+
+        self.d.kill()
+
+    def test_bp_disable(self):
+        d = self.d
+
+        d.run()
+
+        bp1 = d.breakpoint("random_function")
+        bp2 = d.breakpoint(0x40115B)
+        bp3 = d.breakpoint(0x40116D)
+
+        counter = 1
+
+        d.cont()
+
+        while True:
+            d.wait()
+
+            if d.rip == bp1.address:
+                self.assertTrue(bp1.hit_count == 1)
+                self.assertTrue(bp1.hit_on(d))
+                self.assertFalse(bp2.hit_on(d))
+                self.assertFalse(bp3.hit_on(d))
+            elif d.rip == bp2.address:
+                self.assertTrue(bp2.hit_count == counter)
+                self.assertTrue(bp2.hit_on(d))
+                self.assertFalse(bp1.hit_on(d))
+                self.assertFalse(bp3.hit_on(d))
+                bp2.disable()
+            elif d.rip == bp3.address:
+                self.assertTrue(bp3.hit_count == 1)
+                self.assertTrue(d.rsi == 45)
+                self.assertTrue(d.esi == 45)
+                self.assertTrue(d.si == 45)
+                self.assertTrue(d.sil == 45)
+                self.assertTrue(bp3.hit_on(d))
+                self.assertFalse(bp1.hit_on(d))
+                self.assertFalse(bp2.hit_on(d))
+                break
+
+            d.cont()
+
+        self.assertEqual(bp2.hit_count, 1)
+
+        self.d.kill()
+
+    def test_bp_disable_hw(self):
+        d = self.d
+
+        d.run()
+
+        bp1 = d.breakpoint("random_function")
+        bp2 = d.breakpoint(0x40115B, hardware=True)
+        bp3 = d.breakpoint(0x40116D)
+
+        counter = 1
+
+        d.cont()
+
+        while True:
+            d.wait()
+
+            if d.rip == bp1.address:
+                self.assertTrue(bp1.hit_count == 1)
+                self.assertTrue(bp1.hit_on(d))
+                self.assertFalse(bp2.hit_on(d))
+                self.assertFalse(bp3.hit_on(d))
+            elif d.rip == bp2.address:
+                self.assertTrue(bp2.hit_count == counter)
+                self.assertTrue(bp2.hit_on(d))
+                self.assertFalse(bp1.hit_on(d))
+                self.assertFalse(bp3.hit_on(d))
+                bp2.disable()
+            elif d.rip == bp3.address:
+                self.assertTrue(bp3.hit_count == 1)
+                self.assertTrue(d.rsi == 45)
+                self.assertTrue(d.esi == 45)
+                self.assertTrue(d.si == 45)
+                self.assertTrue(d.sil == 45)
+                self.assertTrue(bp3.hit_on(d))
+                self.assertFalse(bp1.hit_on(d))
+                self.assertFalse(bp2.hit_on(d))
+                break
+
+            d.cont()
+
+        self.assertEqual(bp2.hit_count, 1)
+
+    def test_bp_disable_reenable(self):
+        d = self.d
+
+        d.run()
+
+        bp1 = d.breakpoint("random_function")
+        bp2 = d.breakpoint(0x40115B)
+        bp4 = d.breakpoint(0x401162)
+        bp3 = d.breakpoint(0x40116D)
+
+        counter = 1
+
+        d.cont()
+
+        while True:
+            d.wait()
+
+            if d.rip == bp1.address:
+                self.assertTrue(bp1.hit_count == 1)
+                self.assertTrue(bp1.hit_on(d))
+                self.assertFalse(bp2.hit_on(d))
+                self.assertFalse(bp3.hit_on(d))
+            elif d.rip == bp2.address:
+                self.assertTrue(bp2.hit_count == counter)
+                self.assertTrue(bp2.hit_on(d))
+                self.assertFalse(bp1.hit_on(d))
+                self.assertFalse(bp3.hit_on(d))
+                if bp4.enabled:
+                    bp4.disable()
+                else:
+                    bp4.enable()
+                counter += 1
+            elif d.rip == bp3.address:
+                self.assertTrue(bp3.hit_count == 1)
+                self.assertTrue(d.rsi == 45)
+                self.assertTrue(d.esi == 45)
+                self.assertTrue(d.si == 45)
+                self.assertTrue(d.sil == 45)
+                self.assertTrue(bp3.hit_on(d))
+                self.assertFalse(bp1.hit_on(d))
+                self.assertFalse(bp2.hit_on(d))
+                break
+            elif bp4.hit_on(d):
+                pass
+
+            d.cont()
+
+        self.assertEqual(bp4.hit_count, bp2.hit_count // 2 + 1)
+
+        self.d.kill()
+
+    def test_bp_disable_reenable_hw(self):
+        d = self.d
+
+        d.run()
+
+        bp1 = d.breakpoint("random_function")
+        bp2 = d.breakpoint(0x40115B)
+        bp4 = d.breakpoint(0x401162, hardware=True)
+        bp3 = d.breakpoint(0x40116D)
+
+        counter = 1
+
+        d.cont()
+
+        while True:
+            d.wait()
+
+            if d.rip == bp1.address:
+                self.assertTrue(bp1.hit_count == 1)
+                self.assertTrue(bp1.hit_on(d))
+                self.assertFalse(bp2.hit_on(d))
+                self.assertFalse(bp3.hit_on(d))
+            elif d.rip == bp2.address:
+                self.assertTrue(bp2.hit_count == counter)
+                self.assertTrue(bp2.hit_on(d))
+                self.assertFalse(bp1.hit_on(d))
+                self.assertFalse(bp3.hit_on(d))
+                if bp4.enabled:
+                    bp4.disable()
+                else:
+                    bp4.enable()
+                counter += 1
+            elif d.rip == bp3.address:
+                self.assertTrue(bp3.hit_count == 1)
+                self.assertTrue(d.rsi == 45)
+                self.assertTrue(d.esi == 45)
+                self.assertTrue(d.si == 45)
+                self.assertTrue(d.sil == 45)
+                self.assertTrue(bp3.hit_on(d))
+                self.assertFalse(bp1.hit_on(d))
+                self.assertFalse(bp2.hit_on(d))
+                break
+            elif bp4.hit_on(d):
+                pass
+
+            d.cont()
+
+        self.assertEqual(bp4.hit_count, bp2.hit_count // 2 + 1)
+
         self.d.kill()
 
 
