c97ffeb801816968eddba388d75a9ddc859fe6d3
io-no <gabriele18.06.98@gmail.com>
2024-04-20 00:06:01 +0200

    feat: improved color and formatting support

--- libdebug/builtin/pretty_print_syscall_hook.py
+++ libdebug/builtin/pretty_print_syscall_hook.py
@@ -5,64 +5,64 @@
 #
 
 from typing import TYPE_CHECKING
+from libdebug.utils.print_style import PrintStyle
 from libdebug.utils.syscall_utils import (
     resolve_syscall_name,
-    resolve_syscall_number,
     resolve_syscall_arguments,
-    get_all_syscall_numbers,
 )
 
 if TYPE_CHECKING:
     from libdebug.state.thread_context import ThreadContext
 
 
-def enable_pretty_print_syscalls(
-    d: "ThreadContext", syscalls: list[str | int] = None, exclude: list[str | int] = None
-):
-    """Installs a syscall hook that will pretty print the syscall arguments and return value."""
+def pprint_on_enter(d: "ThreadContext", syscall_number: int, **kwargs: bool):
+    """Function that will be called when a syscall is entered in pretty print mode.
+    
+    Args:
+        d (ThreadContext): the thread context.
+        syscall_number (int): the syscall number.
+        **kwargs (bool): the keyword arguments.
+    """
+    syscall_name = resolve_syscall_name(syscall_number)
+    syscall_args = resolve_syscall_arguments(syscall_number)
 
-    def on_enter_syscall(d, syscall_number):
-        syscall_name = resolve_syscall_name(syscall_number)
-        syscall_args = resolve_syscall_arguments(syscall_number)
+    values = [
+        d.syscall_arg0,
+        d.syscall_arg1,
+        d.syscall_arg2,
+        d.syscall_arg3,
+        d.syscall_arg4,
+        d.syscall_arg5,
+    ]
 
-        values = [
-            d.syscall_arg0,
-            d.syscall_arg1,
-            d.syscall_arg2,
-            d.syscall_arg3,
-            d.syscall_arg4,
-            d.syscall_arg5,
-        ]
+    entries = [
+        f"{arg} = {PrintStyle.BRIGHT_YELLOW}0x{value:x}{PrintStyle.DEFAULT_COLOR}"
+        for arg, value in zip(syscall_args, values)
+        if arg is not None
+    ]
+    
+    hijacked = kwargs.get("hijacked", False)
+    user_hooked = kwargs.get("user_hooked", False)
+    if hijacked:
+        print(f"{PrintStyle.RED}(user hijacked) {PrintStyle.STRIKE}{PrintStyle.BLUE}{syscall_name}{PrintStyle.DEFAULT_COLOR}({', '.join(entries)}){PrintStyle.RESET}")
+    elif user_hooked:
+        print(f"{PrintStyle.RED}(user hooked) {PrintStyle.BLUE}{syscall_name}{PrintStyle.DEFAULT_COLOR}({', '.join(entries)}) = ", end="")
+    else:
+        print(f"{PrintStyle.BLUE}{syscall_name}{PrintStyle.DEFAULT_COLOR}({', '.join(entries)}) = ", end="")
+    
 
-        entries = [
-            f"{arg} = 0x{value:x}"
-            for arg, value in zip(syscall_args, values)
-            if arg is not None
-        ]
-
-        print(f"{syscall_name}({', '.join(entries)}) = ", end="")
-
-    def on_exit_syscall(d, _):
-        print(f"0x{d.syscall_return:x}")
-
-    if syscalls is None:
-        syscalls = get_all_syscall_numbers()
-
-    syscall_numbers = []
-
-    for syscall in syscalls:
-        if isinstance(syscall, str):
-            syscall_numbers.append(resolve_syscall_number(syscall))
-        else:
-            syscall_numbers.append(syscall)
-
-    if exclude is not None:
-        for excluded in exclude:
-            if isinstance(excluded, str):
-                excluded = resolve_syscall_number(excluded)
-
-            syscall_numbers.remove(excluded)
-
-    for syscall_number in syscall_numbers:
-        d.hook_syscall(syscall_number, on_enter_syscall, on_exit_syscall)
-
+def pprint_on_exit(d: "ThreadContext", _: int, **kwargs: bool):
+    """Function that will be called when a syscall is exited in pretty print mode.
+    
+    Args:
+        d (ThreadContext): the thread context.
+        _: int: the syscall number.
+        **kwargs (bool): the keyword arguments.
+    """
+    
+    user_hooked = kwargs.get("user_hooked", False)
+    
+    if user_hooked:
+        print(f"{PrintStyle.STRIKE}{PrintStyle.YELLOW}0x{d.syscall_return:x}{PrintStyle.RESET}", end = " ")
+    else:
+        print(f"{PrintStyle.YELLOW}0x{d.syscall_return:x}{PrintStyle.DEFAULT_COLOR}")
