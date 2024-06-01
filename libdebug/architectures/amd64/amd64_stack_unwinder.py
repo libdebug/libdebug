7110e2e2afef643998dda97b58635c655a56175d
MrIndeciso <robertobertolini37@gmail.com>
2024-06-01 12:14:39 +0200

    style: refactor libdebug/architectures submodule

--- libdebug/architectures/amd64/amd64_stack_unwinder.py
+++ libdebug/architectures/amd64/amd64_stack_unwinder.py
@@ -4,6 +4,8 @@
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from libdebug.architectures.stack_unwinding_manager import StackUnwindingManager
@@ -13,13 +15,10 @@ if TYPE_CHECKING:
 
 
 class Amd64StackUnwinder(StackUnwindingManager):
-    """
-    Class that provides stack unwinding for the x86_64 architecture.
-    """
+    """Class that provides stack unwinding for the x86_64 architecture."""
 
-    def unwind(self, target: "ThreadContext") -> list:
-        """
-        Unwind the stack of a process.
+    def unwind(self: Amd64StackUnwinder, target: ThreadContext) -> list:
+        """Unwind the stack of a process.
 
         Args:
             target (ThreadContext): The target ThreadContext.
@@ -27,7 +26,6 @@ class Amd64StackUnwinder(StackUnwindingManager):
         Returns:
             list: A list of return addresses.
         """
-
         assert hasattr(target, "rip")
         assert hasattr(target, "rbp")
 
@@ -53,9 +51,8 @@ class Amd64StackUnwinder(StackUnwindingManager):
 
         return stack_trace
 
-    def get_return_address(self, target: "ThreadContext") -> int:
-        """
-        Get the return address of the current function.
+    def get_return_address(self: Amd64StackUnwinder, target: ThreadContext) -> int:
+        """Get the return address of the current function.
 
         Args:
             target (ThreadContext): The target ThreadContext.
@@ -75,13 +72,10 @@ class Amd64StackUnwinder(StackUnwindingManager):
         else:
             return_address = target.memory[target.rsp + 8, 8]
 
-        return_address = int.from_bytes(return_address, byteorder="little")
+        return int.from_bytes(return_address, byteorder="little")
 
-        return return_address
-
-    def _preamble_state(self, instruction_window: bytes) -> int:
-        """
-        Check if the instruction window is a function preamble and if so at what stage.
+    def _preamble_state(self: Amd64StackUnwinder, instruction_window: bytes) -> int:
+        """Check if the instruction window is a function preamble and if so at what stage.
 
         Args:
             instruction_window (bytes): The instruction window.
@@ -89,17 +83,13 @@ class Amd64StackUnwinder(StackUnwindingManager):
         Returns:
             int: 0 if not a preamble, 1 if rbp has not been pushed yet, 2 otherwise
         """
+        preamble_state = 0
 
-        preambleState = 0
-
-        # endbr64
-        if b"\xf3\x0f\x1e\xfa" in instruction_window:
-            preambleState = 1
-        # push rbp
-        elif b"\x55" in instruction_window:
-            preambleState = 1
+        # endbr64 and push rbp
+        if b"\xf3\x0f\x1e\xfa" in instruction_window or b"\x55" in instruction_window:
+            preamble_state = 1
         # mov rbp, rsp
         elif b"\x48\x89\xe5" in instruction_window:
-            preambleState = 2
+            preamble_state = 2
 
-        return preambleState
+        return preamble_state
--- libdebug/architectures/amd64/amd64_syscall_hijacker.py
+++ libdebug/architectures/amd64/amd64_syscall_hijacker.py
@@ -4,54 +4,65 @@
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
-from typing import Any, Callable
-from libdebug.state.thread_context import ThreadContext
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from libdebug.architectures.syscall_hijacking_manager import SyscallHijackingManager
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from libdebug.state.thread_context import ThreadContext
 
 
 class Amd64SyscallHijacker(SyscallHijackingManager):
     """Class that provides syscall hijacking for the x86_64 architecture."""
 
     # Allowed arguments for the hijacker
-    allowed_args : set[str] = {
-        "syscall_number",
-        "syscall_arg0",
-        "syscall_arg1",
-        "syscall_arg2",
-        "syscall_arg3",
-        "syscall_arg4",
-        "syscall_arg5",
-    }
+    allowed_args: set[str] = frozenset(
+        {
+            "syscall_number",
+            "syscall_arg0",
+            "syscall_arg1",
+            "syscall_arg2",
+            "syscall_arg3",
+            "syscall_arg4",
+            "syscall_arg5",
+        },
+    )
 
     def create_hijacker(
-        self, new_syscall: int, **kwargs: Any
+        self: Amd64SyscallHijacker,
+        new_syscall: int,
+        **kwargs: int,
     ) -> Callable[[ThreadContext, int], None]:
-        """
-        Create a new hijacker for the given syscall.
+        """Create a new hijacker for the given syscall.
 
         Args:
             new_syscall (int): The new syscall number.
             **kwargs: The keyword arguments.
         """
 
-        def hijack_on_enter_wrapper(d: ThreadContext, _: int):
+        def hijack_on_enter_wrapper(d: ThreadContext, _: int) -> None:
             """Wrapper for the hijack_on_enter method."""
             self._hijack_on_enter(d, new_syscall, **kwargs)
 
         return hijack_on_enter_wrapper
 
     def _hijack_on_enter(
-        self, d: ThreadContext, new_syscall: int, **kwargs: Any
+        self: Amd64SyscallHijacker,
+        d: ThreadContext,
+        new_syscall: int,
+        **kwargs: int,
     ) -> None:
-        """
-        Hijack the syscall on enter.
+        """Hijack the syscall on enter.
 
         Args:
             d (ThreadContext): The target ThreadContext.
             new_syscall (int): The new syscall number.
             **kwargs: The keyword arguments.
         """
-
         d.syscall_number = new_syscall
         if "syscall_arg0" in kwargs:
             d.syscall_arg0 = kwargs.get("syscall_arg0", False)
