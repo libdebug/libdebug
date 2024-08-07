edfa28c816fcb043182bef8a7900dc827d815e3b
MrIndeciso <robertobertolini37@gmail.com>
2024-08-07 13:50:07 +0200

    feat: add aarch64 call utilities manager

--- libdebug/architectures/call_utilities_provider.py
+++ libdebug/architectures/call_utilities_provider.py
@@ -1,20 +1,27 @@
 #
 # This file is part of libdebug Python library (https://github.com/libdebug/libdebug).
-# Copyright (c) 2024 Francesco Panebianco. All rights reserved.
+# Copyright (c) 2024 Francesco Panebianco, Roberto Alessandro Bertolini. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
+from libdebug.architectures.aarch64.aarch64_call_utilities import (
+    Aarch64CallUtilities,
+)
 from libdebug.architectures.amd64.amd64_call_utilities import (
     Amd64CallUtilities,
 )
 from libdebug.architectures.call_utilities_manager import CallUtilitiesManager
 
+_aarch64_call_utilities = Aarch64CallUtilities()
 _amd64_call_utilities = Amd64CallUtilities()
+
 
 def call_utilities_provider(architecture: str) -> CallUtilitiesManager:
     """Returns an instance of the call utilities provider to be used by the `_InternalDebugger` class."""
     match architecture:
         case "amd64":
             return _amd64_call_utilities
+        case "aarch64":
+            return _aarch64_call_utilities
         case _:
             raise NotImplementedError(f"Architecture {architecture} not available.")
