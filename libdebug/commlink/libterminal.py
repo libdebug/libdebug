5eca7cf2623ce7358fdc54efa1d2cd4ccd9f8d9d
io-no <gabriele18.06.98@gmail.com>
2024-10-01 23:42:46 +0200

    feat: improvements on the output of the interactive

--- libdebug/commlink/libterminal.py
+++ libdebug/commlink/libterminal.py
@@ -14,21 +14,18 @@ from queue import Queue
 from threading import Event
 from typing import TYPE_CHECKING
 
-from prompt_toolkit.application import Application
-from prompt_toolkit.filters.base import Condition
+from prompt_toolkit.application import Application, run_in_terminal
 from prompt_toolkit.history import FileHistory, InMemoryHistory
 from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.layout import HSplit, Layout
-from prompt_toolkit.styles import Style
+from prompt_toolkit.layout import Layout
 from prompt_toolkit.widgets import TextArea
 
-from libdebug.commlink.logging_lexer import LoggingLexer
 from libdebug.commlink.std_wrapper import StdWrapper
 from libdebug.debugger.internal_debugger_instance_manager import provide_internal_debugger
 from libdebug.liblog import liblog
 
 if TYPE_CHECKING:
-    from prompt_toolkit.application import KeyPressEvent
+    from prompt_toolkit.application.application import KeyPressEvent
 
 PATH_HISTORY = Path.home() / ".cache" / "libdebug" / "history"
 
@@ -49,9 +46,6 @@ class LibTerminal:
 
         # Initialize the message queue for the prompt_toolkit application
         self._app_message_queue: Queue = Queue()
-
-        # Initialize the prompt_toolkit application reference
-        self._app: Application | None = None
 
         # Initialize the thread reference for the prompt_toolkit application
         self._app_thread: threading.Thread | None = None
@@ -93,27 +87,10 @@ class LibTerminal:
 
     def _run_prompt(self: LibTerminal, prompt: str) -> None:
         """Run the prompt_toolkit application."""
-        header_text = "Ctrl+C/Ctrl+D: Exit | Tab: Switch Focus | Ctrl+S: Mouse Scroll\n"
-
-        header = TextArea(
-            text=header_text,
-            style="class:header",
-            height=1,
-            focusable=False,
-            read_only=True,
-        )
-        output_field = TextArea(
-            style="class:output-field",
-            focusable=True,
-            scrollbar=True,
-            lexer=LoggingLexer(),
-        )
         input_field = TextArea(
             height=3,
             prompt=prompt,
             style="class:input-field",
-            focusable=True,
-            history=InMemoryHistory(),
         )
 
         kb = KeyBindings()
@@ -148,55 +125,13 @@ class LibTerminal:
             event.app.exit()
             sys.exit(0)
 
-        # Switch focus to output_field
-        @kb.add("c-o")
-        def focus_output(event: KeyPressEvent) -> None:
-            """Switch focus to the output field."""
-            event.app.layout.focus(output_field)
+        layout = Layout(input_field)
 
-        # Switch focus to input_field
-        @kb.add("c-i")
-        def focus_input(event: KeyPressEvent) -> None:
-            """Switch focus to the input field."""
-            event.app.layout.focus(input_field)
-
-        @kb.add("tab")
-        def focus_next(event: KeyPressEvent) -> None:
-            """Switch focus to the next field."""
-            event.app.layout.focus_next()
-
-        @Condition
-        def mouse_support_on() -> bool:
-            """Check if the mouse support is enabled."""
-            return self._mouse_support
-
-        @kb.add("c-s")
-        def enable_mouse_support(_: KeyPressEvent) -> None:
-            """Enable mouse scroll support."""
-            self._mouse_support = not self._mouse_support
-
-        layout = Layout(HSplit([header, output_field, input_field]), focused_element=input_field)
-
-        # Define the style for the prompt_toolkit application to correctly display the log messages
-        style = Style.from_dict(
-            {
-                "output-field": "",
-                "input-field": "",
-                "warning": "fg:orange",
-                "error": "fg:red",
-                "info": "fg:green",
-                "header": "fg:#FF6500 bold",
-            },
-        )
-
-        self._app = Application(
+        app = Application(
             layout=layout,
             key_bindings=kb,
             full_screen=False,
             refresh_interval=0.5,
-            style=style,
-            erase_when_done=True,
-            mouse_support=mouse_support_on,
         )
 
         def update_output(app: Application) -> None:
@@ -218,29 +153,29 @@ class LibTerminal:
                     pass
 
             # Update the output field with the messages in the queue
-            msg = ""
-            while not self._app_message_queue.empty():
+            msg = b""
+            if not self._app_message_queue.empty():
                 msg += self._app_message_queue.get()
-            output_field.buffer.insert_text(msg)
-            self._output_buffer += msg
+
+            if msg:
+                run_in_terminal(lambda: sys.stdout.buffer.write(msg))
 
             if to_exit:
                 app.exit()
-                sys.exit(0)
 
         # Add the update_output function to the event loop
-        self._app.on_invalidate.add_handler(update_output)
+        app.on_invalidate.add_handler(update_output)
 
         # Run in another thread
-        self._app_thread = threading.Thread(target=self._app.run, daemon=True)
+        self._app_thread = threading.Thread(target=app.run, daemon=True)
         self._app_thread.start()
 
     def _write_manager(self, payload: bytes) -> int:
         """Put the payload in the message queue for the prompt_toolkit application."""
         if isinstance(payload, bytes):
-            self._app_message_queue.put(payload.decode("ascii", errors="backslashreplace"))
+            self._app_message_queue.put(payload.decode("ascii", errors="backslashreplace").encode("ascii"))
         else:
-            self._app_message_queue.put(payload)
+            self._app_message_queue.put(payload.encode("ascii", errors="backslashreplace"))
 
     def reset(self: LibTerminal) -> None:
         """Reset the terminal to its original state."""
