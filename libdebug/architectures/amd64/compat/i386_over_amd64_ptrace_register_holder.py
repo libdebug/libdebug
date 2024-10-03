4ee152213027630d5fb291bfc7001579ab8429c3
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2024-10-03 11:02:34 +0200

    refactor: use inheritance to avoid duplicating code

--- libdebug/architectures/amd64/compat/i386_over_amd64_ptrace_register_holder.py
+++ libdebug/architectures/amd64/compat/i386_over_amd64_ptrace_register_holder.py
@@ -14,44 +14,20 @@ from libdebug.architectures.amd64.amd64_ptrace_register_holder import (
     _get_property_8l,
     _get_property_16,
     _get_property_32,
-    _get_property_fp_mmx,
-    _get_property_fp_st,
-    _get_property_fp_xmm0,
-    _get_property_fp_ymm0,
-    _get_property_fp_zmm0,
 )
 from libdebug.architectures.amd64.compat.i386_over_amd64_registers import I386OverAMD64Registers
-from libdebug.ptrace.ptrace_register_holder import PtraceRegisterHolder
+from libdebug.architectures.i386.i386_ptrace_register_holder import (
+    I386_BASE_REGS,
+    I386_GP_REGS,
+    I386PtraceRegisterHolder,
+)
 
 if TYPE_CHECKING:
     from libdebug.state.thread_context import ThreadContext
 
-I386_GP_REGS = ["a", "b", "c", "d"]
-
-I386_BASE_REGS = ["bp", "sp", "si", "di"]
-
-I386_REGS = [
-    "eax",
-    "ebx",
-    "ecx",
-    "edx",
-    "esi",
-    "edi",
-    "ebp",
-    "esp",
-    "eip",
-    "eflags",
-    "cs",
-    "ss",
-    "ds",
-    "es",
-    "fs",
-    "gs",
-]
-
 
 @dataclass
-class I386OverAMD64PtraceRegisterHolder(PtraceRegisterHolder):
+class I386OverAMD64PtraceRegisterHolder(I386PtraceRegisterHolder):
     """A class that provides views and setters for the registers of an x86_64 process."""
 
     def provide_regs_class(self: I386OverAMD64PtraceRegisterHolder) -> type:
@@ -135,57 +111,3 @@ class I386OverAMD64PtraceRegisterHolder(PtraceRegisterHolder):
         target_class.syscall_arg3 = _get_property_32("rsi")
         target_class.syscall_arg4 = _get_property_32("rdi")
         target_class.syscall_arg5 = _get_property_32("rbp")
-
-    def provide_regs(self: I386OverAMD64PtraceRegisterHolder) -> list[str]:
-        """Provide the list of registers, excluding the vector and fp registers."""
-        return I386_REGS
-
-    def provide_vector_fp_regs(self: I386OverAMD64PtraceRegisterHolder) -> list[str]:
-        """Provide the list of vector and floating point registers."""
-        return self._vector_fp_registers
-
-    def _handle_fp_legacy(self: I386OverAMD64PtraceRegisterHolder, target_class: type) -> None:
-        """Handle legacy mmx and st registers."""
-        for index in range(8):
-            name_mm = f"mm{index}"
-            setattr(target_class, name_mm, _get_property_fp_mmx(name_mm, index))
-
-            name_st = f"st{index}"
-            setattr(target_class, name_st, _get_property_fp_st(name_st, index))
-
-            self._vector_fp_registers.append((name_mm, name_st))
-
-    def _handle_vector_512(self: I386OverAMD64PtraceRegisterHolder, target_class: type) -> None:
-        """Handle the case where the xsave area is 512 bytes long, which means we just have the xmm registers."""
-        # i386 only gets 8 registers
-        for index in range(8):
-            name_xmm = f"xmm{index}"
-            setattr(target_class, name_xmm, _get_property_fp_xmm0(name_xmm, index))
-            self._vector_fp_registers.append((name_xmm,))
-
-    def _handle_vector_896(self: I386OverAMD64PtraceRegisterHolder, target_class: type) -> None:
-        """Handle the case where the xsave area is 896 bytes long, which means we have the xmm and ymm registers."""
-        # i386 only gets 8 registers
-        for index in range(8):
-            name_xmm = f"xmm{index}"
-            setattr(target_class, name_xmm, _get_property_fp_xmm0(name_xmm, index))
-
-            name_ymm = f"ymm{index}"
-            setattr(target_class, name_ymm, _get_property_fp_ymm0(name_ymm, index))
-
-            self._vector_fp_registers.append((name_xmm, name_ymm))
-
-    def _handle_vector_2696(self: I386OverAMD64PtraceRegisterHolder, target_class: type) -> None:
-        """Handle the case where the xsave area is 2696 bytes long, which means we have 32 zmm registers."""
-        # i386 only gets 8 registers
-        for index in range(8):
-            name_xmm = f"xmm{index}"
-            setattr(target_class, name_xmm, _get_property_fp_xmm0(name_xmm, index))
-
-            name_ymm = f"ymm{index}"
-            setattr(target_class, name_ymm, _get_property_fp_ymm0(name_ymm, index))
-
-            name_zmm = f"zmm{index}"
-            setattr(target_class, name_zmm, _get_property_fp_zmm0(name_zmm, index))
-
-            self._vector_fp_registers.append((name_xmm, name_ymm, name_zmm))
