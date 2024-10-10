9cb2311acf32f93495b985c6d9b158c326400bf7
io-no <gabriele18.06.98@gmail.com>
2024-10-10 14:27:02 +0200

    test: new fantastic benchmarks

---
+++ test/benchmarks/memory_gdb.py
@@ -0,0 +1,78 @@
+#
+# This file is part of libdebug Python library (https://github.com/libdebug/libdebug).
+# Copyright (c) 2024 Gabriele Digregorio. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
+import gdb
+from time import perf_counter
+import pickle
+
+class MyBreakpoint(gdb.Breakpoint):
+    """ Class to handle the breakpoint action """
+    def __init__(self, spec):
+        """ Initialize a hardware breakpoint """
+        super(MyBreakpoint, self).__init__(spec, gdb.BP_HARDWARE_BREAKPOINT)
+        self.silent = True
+        
+    def stop(self):
+        """ Callback function to be called at each breakpoint hit """
+        global start, end
+        
+        # Start the timer
+        start = perf_counter()
+        
+        # Access to RSP register to get the address
+        address = gdb.selected_frame().read_register("rsp") - 0x10000
+        
+        # Read the memory
+        gdb.selected_inferior().read_memory(address, 0x10000)
+        
+        # Stop the timer
+        end = perf_counter()
+        
+class Debugger(gdb.Command):
+    """ Class to handle the debugging session """
+    def __init__(self):
+        super(Debugger, self).__init__("memory_gdb", gdb.COMMAND_USER)
+        
+    def test(self):
+        """ This test includes the time to:
+        - run the debugged process from the entrypoint,
+        - hit the breakpoint 1000 times,
+        - each time the breakpoint is hit, retrieve the RSP register and read the memory,
+        - wait the process to end.
+        """
+        gdb.execute("set pagination off")
+
+        # Start the process (it will stop at the entrypoint)
+        gdb.execute("start")
+        
+        # Set the hardware breakpoint
+        MyBreakpoint("*0x401302")
+        
+        # Continue the process from the entrypoint and wait for the process to end
+        gdb.execute("continue")
+
+        # Delete the breakpoints
+        gdb.execute("del breakpoints")
+        
+        self.results.append(end-start)
+
+    def invoke(self, arg, from_tty):
+        # Initialize the results
+        self.results = []
+        
+        # Load the binary
+        binary = "../binaries/amd64/math_loop_test"
+        gdb.execute(f"file {binary}")
+        
+        for _ in range(1000):
+            self.test()
+
+        # Save the result in a pickle file
+        with open("breakpoint_gdb.pkl", "wb") as f:
+            pickle.dump(self.results, f)
+        # print("Results:", self.results)
+                
+Debugger()
---
+++ test/benchmarks/memory_libdebug.py
@@ -0,0 +1,68 @@
+#
+# This file is part of libdebug Python library (https://github.com/libdebug/libdebug).
+# Copyright (c) 2024 Gabriele Digregorio. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
+from time import perf_counter
+import pickle
+from libdebug import debugger
+
+
+def callback(t,b):
+    """ Callback function to be called at each breakpoint hit """
+    global start, end
+    
+    # Start the timer
+    start = perf_counter()
+    
+    # Access to RSP register to get the address
+    address = t.regs.rsp - 0x10000
+    
+    # Read the memory
+    t.memory[address, 0x10000, "absolute"]
+    
+    # Stop the timer
+    end = perf_counter()
+        
+def test():
+    """ This test includes the time to:
+    - run the debugged process from the entrypoint,
+    - hit the breakpoint 1000 times,
+    - each time the breakpoint is hit, retrieve the RSP register and read the memory,
+    - wait the process to end.
+    """    
+    # Start the process (it will stop at the entrypoint)
+    d.run()     
+
+    # Set the hardware breakpoint     
+    d.breakpoint(0x401302, callback=callback, hardware=True, file="absolute")
+    
+    # Continue the process from the entrypoint
+    d.cont()
+
+    # Wait for the process to end
+    d.wait()
+    
+    # Kill for a clean exit
+    d.kill()
+
+    results.append(end-start)
+
+# Initialize the results
+results = []
+
+# Initialize the debugger
+d = debugger("../binaries/amd64/math_loop_test", fast_memory=True)
+
+for _ in range(1000):
+    test()
+
+# Terminate the debugger
+d.terminate()
+
+# Save the result in a pickle file
+with open("breakpoint_libdebug.pkl", "wb") as f:
+    pickle.dump(results, f)
+
+# print("Results:", results)
