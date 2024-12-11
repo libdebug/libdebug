8898cffcec3273e73b6d196354d2a313560b0b66
io-no <gabriele18.06.98@gmail.com>
2024-12-11 22:59:33 +0100

    test: added test on cohesistent multiple bps both hw and sw

--- test/scripts/breakpoint_test.py
+++ test/scripts/breakpoint_test.py
@@ -768,7 +768,7 @@ class BreakpointTest(TestCase):
     
     def test_bp_async_sw_thread_scoped(self):
         def callback(t, bp):
-            # self.assertEqual(t.thread_id, target.thread_id)
+            self.assertEqual(t.thread_id, target.thread_id)
             pass
         
         d = debugger(RESOLVE_EXE("multithread_input"))
@@ -956,6 +956,103 @@ class BreakpointTest(TestCase):
 
         d.kill()
         d.terminate()
+        
+    def test_multiple_bps_async_both_sw_hw(self):
+        counter_1_1, counter_1_2 = 0, 0
+        counter_2_1, counter_2_2 = 0, 0
+        counter_3_1, counter_3_2 = 0, 0
+        
+        def first_first(t, bp):
+            nonlocal counter_1_1
+            counter_1_1 += 1
+            
+            # This callback should be called before the second one
+            self.assertGreater(counter_1_1, counter_1_2)
+            
+        def first_second(t, bp):
+            nonlocal counter_1_2
+            counter_1_2 += 1
+            
+            # This callback should be called after the first one
+            self.assertTrue(counter_1_2 == counter_1_1 or counter_1_2 == counter_1_1 - 1)
+            
+        def second_first(t, bp):
+            nonlocal counter_2_1
+            counter_2_1 += 1
+            
+            # This callback should be called before the second one
+            self.assertGreater(counter_2_1, counter_2_2)
+            
+        def second_second(t, bp):
+            nonlocal counter_2_2
+            counter_2_2 += 1
+            
+            # This callback should be called after the first one
+            self.assertTrue(counter_2_2 == counter_2_1 or counter_2_2 == counter_2_1 - 1)
+            
+        def third_first(t, bp):
+            nonlocal counter_3_1
+            counter_3_1 += 1
+            
+            # This callback should be called before the second one
+            self.assertGreater(counter_3_1, counter_3_2)
+            
+        def third_second(t, bp):
+            nonlocal counter_3_2
+            counter_3_2 += 1
+            
+            # This callback should be called after the first one
+            self.assertTrue(counter_3_2 == counter_3_1 or counter_3_2 == counter_3_1 - 1)
+    
+        d = debugger(RESOLVE_EXE("breakpoint_test"))
+
+        d.run()
+
+        bp1_1 = d.breakpoint("random_function", callback=first_first)
+        bp1_1_hw = d.breakpoint("random_function", callback=first_first, hardware=True)
+        bp1_2 = d.breakpoint("random_function", callback=first_second)
+        bp1_2_hw = d.breakpoint("random_function", callback=first_second, hardware=True)
+        bp2_1 = d.breakpoint(TEST_BPS_ADDRESS_1, callback=second_first)
+        bp2_1_hw = d.breakpoint(TEST_BPS_ADDRESS_1, callback=second_first, hardware=True)
+        bp2_2 = d.breakpoint(TEST_BPS_ADDRESS_1, callback=second_second)
+        bp2_2_hw = d.breakpoint(TEST_BPS_ADDRESS_1, callback=second_second, hardware=True)
+        bp3_1 = d.breakpoint(TEST_BPS_ADDRESS_2, callback=third_first)
+        bp3_1_hw = d.breakpoint(TEST_BPS_ADDRESS_2, callback=third_first, hardware=True)
+        bp3_2 = d.breakpoint(TEST_BPS_ADDRESS_2, callback=third_second)
+        bp3_2_hw = d.breakpoint(TEST_BPS_ADDRESS_2, callback=third_second, hardware=True)
+
+        d.cont()
+        
+        d.wait()
+
+        self.assertEqual(counter_1_1, 2)
+        self.assertEqual(counter_1_2, 2)
+        self.assertEqual(counter_2_1, 20)
+        self.assertEqual(counter_2_2, 20)
+        self.assertEqual(counter_3_1, 2)
+        self.assertEqual(counter_3_2, 2)
+        
+        self.assertEqual(bp1_1.hit_count, counter_1_1 // 2)
+        self.assertEqual(bp1_2.hit_count, counter_1_2 // 2)
+        self.assertEqual(bp2_1.hit_count, counter_2_1 // 2)
+        self.assertEqual(bp2_2.hit_count, counter_2_2 // 2)
+        self.assertEqual(bp3_1.hit_count, counter_3_1 // 2)
+        self.assertEqual(bp3_2.hit_count, counter_3_2 // 2)
+        
+        self.assertEqual(bp1_1_hw.hit_count, counter_1_1 // 2)
+        self.assertEqual(bp1_2_hw.hit_count, counter_1_2 // 2)
+        self.assertEqual(bp2_1_hw.hit_count, counter_2_1 // 2)
+        self.assertEqual(bp2_2_hw.hit_count, counter_2_2 // 2)
+        self.assertEqual(bp3_1_hw.hit_count, counter_3_1 // 2)
+        self.assertEqual(bp3_2_hw.hit_count, counter_3_2 // 2)
+        
+        
+        self.assertEqual(d.breakpoints[bp1_1.address].hit_count, 4)
+        self.assertEqual(d.breakpoints[TEST_BPS_ADDRESS_1].hit_count, 40)
+        self.assertEqual(d.breakpoints[TEST_BPS_ADDRESS_2].hit_count, 4)
+
+        d.kill()
+        d.terminate()
     
     
     def test_multiple_bps_disable(self):
@@ -1039,4 +1136,45 @@ class BreakpointTest(TestCase):
         self.assertEqual(d.breakpoints[TEST_BPS_ADDRESS_2].hit_count, 2)
 
         d.kill()
+        d.terminate()
+    
+    def test_max_number_hw_bp_scoped(self):
+        def callback(t, bp):
+            self.assertEqual(t.thread_id, target.thread_id)
+            pass
+        
+        d = debugger(RESOLVE_EXE("multithread_input"))
+        
+        r = d.run()
+        
+        d.cont()
+        
+        # Let wait all threads to be created
+        r.recvuntil(b"All threads have been created.")
+        
+        # Interrupt the process
+        d.interrupt()
+        
+        # Choice a target
+        target = d.threads[2]
+        other_threads = d.threads.copy()
+        other_threads.remove(target)
+        
+        # Set bps on the target thread until we finish the hardware breakpoints
+        offset = 0
+        while True:
+            try:
+                bp = target.bp(TEST_THREAD_SCOPED_END_THREAD + offset, file="binary", callback=callback, hardware=True)
+            except ValueError:
+                break
+            offset += 0x10
+            
+        # At this point we should be able to set sw breakpoints on the same thread
+        bp = target.bp(TEST_THREAD_SCOPED_END_THREAD + offset, file="binary", callback=callback)
+        
+        # We should also be able to set hw breakpoints on other threads
+        for thread in other_threads:
+            thread.bp(TEST_THREAD_SCOPED_END_THREAD, file="binary", hardware=True)
+        
+        d.kill()        
         d.terminate()
