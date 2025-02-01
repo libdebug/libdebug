473e5b566adea06bd5682f81e0eb515e5c859598
io-no <gabriele18.06.98@gmail.com>
2025-02-01 14:28:56 +0100

    doc: updated the copyright

--- libdebug/native/symbols/debug_sym_parser.cpp
+++ libdebug/native/symbols/debug_sym_parser.cpp
@@ -1,6 +1,6 @@
 //
 // This file is part of libdebug Python library (https://github.com/libdebug/libdebug).
-// Copyright (c) 2023-2024 Gabriele Digregorio, Roberto Alessandro Bertolini. All rights reserved.
+// Copyright (c) 2023-2025 Gabriele Digregorio, Roberto Alessandro Bertolini. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 //
 
