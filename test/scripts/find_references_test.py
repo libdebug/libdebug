6966970c74dab2e3cc96dabe381674825ee13996
io-no <gabriele18.06.98@gmail.com>
2025-02-03 01:57:52 +0100

    fix test: mistke in the bp address

--- test/scripts/find_references_test.py
+++ test/scripts/find_references_test.py
@@ -26,7 +26,7 @@ class FindReferencesTest(TestCase):
         d = debugger(RESOLVE_EXE("find_ref_test"))
         
         r = d.run()
-        d.bp(0x12c4, hardware=True, file="binary")
+        d.bp(LOCATION, hardware=True, file="binary")
 
         d.cont()
 
