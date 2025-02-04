a9e5453cd5b44d3cf0b30ce2e690ee7b49d647e9
io-no <gabriele18.06.98@gmail.com>
2025-02-04 10:36:19 +0100

    doc: copyright, ooops

--- test/srcs/find_ref_test.c
+++ test/srcs/find_ref_test.c
@@ -1,3 +1,8 @@
+//
+// This file is part of libdebug Python library (https://github.com/libdebug/libdebug).
+// Copyright (c) 2025 Gabriele Digregorio. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+//
 #include <stdio.h>
 #include <stdlib.h>
 
