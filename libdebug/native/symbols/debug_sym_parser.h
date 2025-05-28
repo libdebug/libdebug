f7b0694ba1a0b478a42f1bc33c37efc46203d36a
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2025-05-28 15:51:42 +0200

    build: add partial support for static linking of libelf and libdwarf

--- libdebug/native/symbols/debug_sym_parser.h
+++ libdebug/native/symbols/debug_sym_parser.h
@@ -6,12 +6,31 @@
 
 #pragma once
 
-#include "debug_sym_parser_shared.h"
-
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <cstring>
 #include <dwarf.h>
+#include <unistd.h>
 #include <gelf.h>
 #include <libdwarf.h>
 #include <libelf.h>
+
+struct SymbolInfo
+{
+    std::string name;
+    unsigned long long high_pc;
+    unsigned long low_pc;
+};
+
+using SymbolVector = std::vector<SymbolInfo>;
+
+struct ElfInfo
+{
+    std::string build_id;
+    std::string debuglink;
+    SymbolVector symbols;
+};
 
 void add_symbol_info(SymbolVector &, const char *, const Dwarf_Addr, const Dwarf_Addr);
 void process_symbol_tables(Elf *, SymbolVector &);
