92f82184b996afb14504ab92fc46949e481f9890
io-no <gabriele18.06.98@gmail.com>
2025-05-29 01:49:30 +0200

    fix: highpc is now computed correclty

--- libdebug/native/symbols/debug_sym_parser_legacy.cpp
+++ libdebug/native/symbols/debug_sym_parser_legacy.cpp
@@ -19,7 +19,7 @@ struct DwarfHandle {
     Dwarf_Debug dbg{nullptr};
     explicit DwarfHandle(int fd) {
         if (dwarf_init(fd, DW_DLC_READ, nullptr, nullptr, &dbg, nullptr) != DW_DLV_OK)
-        throw std::runtime_error("Failed to initialize the DWARF library");
+            throw std::runtime_error("Failed to initialize the DWARF library");
     }
     DwarfHandle             (const DwarfHandle&) = delete;
     DwarfHandle& operator = (const DwarfHandle&) = delete;
@@ -38,26 +38,64 @@ public:
 
 
 //─────────────────────────── Code Implementation ──────────────────────────//
+void parse_highpc(Dwarf_Attribute highpc_attr, Dwarf_Addr &highpc, Dwarf_Addr &lowpc)
+{   
+    Dwarf_Error error = nullptr;
+    Dwarf_Half dw_return_form = 0;
+
+    // Get the form of the highpc attribute
+    if (dwarf_whatform(highpc_attr, &dw_return_form, &error) != DW_DLV_OK) {
+        throw std::runtime_error("Failed to get the form of the highpc attribute");
+    }
+
+    // Check if the highpc is in address form or unit data form
+    switch(dw_return_form) {
+        #ifdef DW_FORM_addrx
+        case DW_FORM_addrx:
+        #endif
+        case DW_FORM_addr:
+            if (dwarf_formaddr(highpc_attr, &highpc, &error) != DW_DLV_OK) {
+                throw std::runtime_error("Failed to get the highpc address");
+            }
+            break;
+        case DW_FORM_data4:
+        case DW_FORM_data8:
+            if (dwarf_formudata(highpc_attr, &highpc, &error) != DW_DLV_OK) {
+                throw std::runtime_error("Failed to get the highpc value");
+            }
+            highpc += lowpc;
+            break;
+        default:
+            throw std::runtime_error("Unsupported form for highpc attribute");
+    }
+}
+
+
 void process_subprogram(Dwarf_Debug dbg, Dwarf_Die die, SymbolVector &symbols)
 {
     Dwarf_Error error = nullptr;
     char *die_name = nullptr;
     Dwarf_Addr lowpc = 0, highpc = 0;
-    Dwarf_Half dw_return_form = 0;
-    enum Dwarf_Form_Class dw_return_class = DW_FORM_CLASS_UNKNOWN;
+    Dwarf_Attribute highpc_attr = nullptr;
 
     if (dwarf_diename(die, &die_name, &error) == DW_DLV_OK) {
         DwarfAutoFree<char*, DW_DLA_STRING> name_guard(dbg, die_name); // RAII
+
+        if (die_name == nullptr || *die_name == '\0') {
+            // If the name is empty, we skip this subprogram
+            return;
+        }
+
         if (dwarf_lowpc(die, &lowpc, &error) == DW_DLV_OK &&
-            dwarf_highpc_b(die, &highpc, &dw_return_form, &dw_return_class, &error) == DW_DLV_OK) {
-            // Check if the highpc is in address form or unit data form
-            if (dw_return_class == DW_FORM_CLASS_CONSTANT){
-                // If highpc is a constant, it is an offset from lowpc
-                highpc += lowpc;
-            }
-                // Add the symbol to the symbols vector
-                add_symbol_info(symbols, die_name, lowpc, highpc);
-            }
+                dwarf_attr(die, DW_AT_high_pc, &highpc_attr, &error) == DW_DLV_OK) {
+                
+            DwarfAutoFree<Dwarf_Attribute, DW_DLA_ATTR> attr_guard(dbg, highpc_attr); // RAII
+            
+            parse_highpc(highpc_attr, highpc, lowpc);
+
+            // Add the symbol to the symbols vector
+            add_symbol_info(symbols, die_name, lowpc, highpc);
+        }
     }
 }
 
@@ -66,24 +104,29 @@ void process_variable(Dwarf_Debug dbg, Dwarf_Die die, SymbolVector &symbols)
     Dwarf_Error error = nullptr;
     char *die_name = nullptr;
     Dwarf_Addr lowpc = 0, highpc = 0;
-    Dwarf_Half dw_return_form = 0;
-    enum Dwarf_Form_Class dw_return_class = DW_FORM_CLASS_UNKNOWN;
     Dwarf_Attribute loc_attr = nullptr;
+    Dwarf_Attribute highpc_attr = nullptr;
+
 
     if (dwarf_diename(die, &die_name, &error) == DW_DLV_OK) {
         DwarfAutoFree<char*, DW_DLA_STRING> name_guard(dbg, die_name); // RAII
 
+        if (die_name == nullptr || *die_name == '\0') {
+            // If the name is empty, we skip this variable
+            return;
+        }
+
         // Some variables, like the global variables, may still have a lowpc and highpc
         // Other variables, like local variables, may not have a lowpc and highpc but an at_location attribute
         if (dwarf_lowpc(die, &lowpc, &error) == DW_DLV_OK &&
-            dwarf_highpc_b(die, &highpc, &dw_return_form, &dw_return_class, &error) == DW_DLV_OK) {
-                // Check if the highpc is in address form or unit data form
-                if (dw_return_class == DW_FORM_CLASS_CONSTANT){
-                    // If highpc is a constant, it is an offset from lowpc
-                    highpc += lowpc;
-                }
-                    // Add the symbol to the symbols vector
-                    add_symbol_info(symbols, die_name, lowpc, highpc);
+                    dwarf_attr(die, DW_AT_high_pc, &highpc_attr, &error) == DW_DLV_OK) {
+                
+            DwarfAutoFree<Dwarf_Attribute, DW_DLA_ATTR> attr_guard(dbg, highpc_attr); // RAII
+            
+            parse_highpc(highpc_attr, highpc, lowpc);
+
+            // Add the symbol to the symbols vector
+            add_symbol_info(symbols, die_name, lowpc, highpc);
         } else if (dwarf_attr(die, DW_AT_location, &loc_attr, &error) == DW_DLV_OK){
             DwarfAutoFree<Dwarf_Attribute, DW_DLA_ATTR> attr_guard(dbg, loc_attr); // RAII
             add_symbol_info(symbols, die_name, 0, 0);
@@ -92,25 +135,20 @@ void process_variable(Dwarf_Debug dbg, Dwarf_Die die, SymbolVector &symbols)
 }
 
 
-void process_die(Dwarf_Debug dbg, Dwarf_Die the_die, SymbolVector &symbols)
+void process_die(Dwarf_Debug dbg, Dwarf_Die die, SymbolVector &symbols)
 {
     Dwarf_Error error;
     Dwarf_Half tag;
-    char *die_name = 0;
-    Dwarf_Addr lowpc = 0, highpc = 0;
-    Dwarf_Attribute *attrs;
-    Dwarf_Signed attrcount, i;
-    int is_formaddr = -1;
 
-    if (dwarf_tag(the_die, &tag, &error) != DW_DLV_OK) {
+    if (dwarf_tag(die, &tag, &error) != DW_DLV_OK) {
         throw std::runtime_error("Failed to get the tag of the DIE");
     }
 
     // Check if the DIE is a subprogram (function) or a variable
     if (tag == DW_TAG_subprogram) {
-        process_subprogram(dbg, the_die, symbols);
+        process_subprogram(dbg, die, symbols);
     } else if (tag == DW_TAG_variable || tag == DW_TAG_formal_parameter) {
-        process_variable(dbg, the_die, symbols);
+        process_variable(dbg, die, symbols);
     } 
 }
 
@@ -156,7 +194,7 @@ void dwarf_retrieve_symbol_names(Dwarf_Debug dbg, SymbolVector &symbols)
     Dwarf_Error err = nullptr;
     Dwarf_Unsigned cu_header_length = 0, abbrev_offset = 0, next_cu_header = 0;
     Dwarf_Half version_stamp = 0, address_size = 0;
-    Dwarf_Die cu_die = nullptr, child_die = nullptr, sibling_die = nullptr;
+    Dwarf_Die cu_die = nullptr;
 
     // Loop through all the compilation units. This call will set the inner cursor to the first CU.
     while (dwarf_next_cu_header(dbg, &cu_header_length, &version_stamp, &abbrev_offset, &address_size, 
