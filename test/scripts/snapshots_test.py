ceeac3ae4a428c0449e0840b68aa970ff046d663
io-no <gabriele18.06.98@gmail.com>
2025-06-22 15:41:32 +0200

    doc: mannaggia to the copyright

--- test/scripts/snapshots_test.py
+++ test/scripts/snapshots_test.py
@@ -1,6 +1,6 @@
 #
 # This file is part of libdebug Python library (https://github.com/libdebug/libdebug).
-# Copyright (c) 2025 Francesco Panebianco, Roberto Alessandro Bertolini, Gabriele Digregorio. All rights reserved.
+# Copyright (c) 2024-2025 Francesco Panebianco, Roberto Alessandro Bertolini, Gabriele Digregorio. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
