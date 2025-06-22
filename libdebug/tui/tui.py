c12fc30d38e1dc3b45a6d296b63c0ec0ed260141
io-no <gabriele18.06.98@gmail.com>
2025-06-23 01:20:02 +0200

    feat: added ipython!!!!!

--- libdebug/tui/tui.py
+++ libdebug/tui/tui.py
@@ -1,22 +1,25 @@
 from __future__ import annotations
 
+import sys
 from typing import TYPE_CHECKING
+
+from IPython import embed
 
 from libdebug.utils.libcontext import libcontext
 
 if TYPE_CHECKING:
-    from libdebug.debugger.debugger import Debugger
+    from libdebug.debugger.internal_debugger import InternalDebugger
 
 from libdebug.utils.pprint_primitives import pprint_backtrace_util, pprint_memory_util, pprint_registers_util
 
 # TODO: select the right thread to display registers and stack
 
 
-def draw_registers(dbg: Debugger) -> None:
+def draw_registers(dbg: InternalDebugger) -> None:
     """Render the registers in a reader-friendly format."""
     print("┌─[ registers ]")
     pprint_registers_util(
-        dbg.regs,
+        dbg.threads[0].regs,
         dbg.maps,
         dbg.threads[0]._register_holder.provide_regs(),
         start_char="│ ",
@@ -24,25 +27,24 @@ def draw_registers(dbg: Debugger) -> None:
     print("└─")
 
 
-def draw_backtrace(dbg: Debugger) -> None:
+def draw_backtrace(dbg: InternalDebugger) -> None:
     """Draw the backtrace of the current thread."""
     print("┌─[ backtrace ]")
     # We do not want annoying warning about broken backtrace here
     with libcontext.tmp(general_logger="SILENT"):
-        backtrace = dbg.backtrace()
+        backtrace = dbg.threads[0].backtrace()
         maps = dbg.maps
         pprint_backtrace_util(backtrace=backtrace, maps=maps, external_symbols=dbg.symbols, start_char="│ ")
     print("└─")
 
 
-def draw_stack(dbg: Debugger) -> None:
+def draw_stack(dbg: InternalDebugger) -> None:
     """Draw the stack in a reader-friendly format."""
-    sp = dbg.regs.__getattribute__("rsp")  # stack pointer
-    mem = dbg.mem[sp, 8 * 8]  # top 8 qwords
+    sp = dbg.threads[0].regs.__getattribute__("rsp")  # stack pointer
     print("┌─[ stack ]")
     pprint_memory_util(
         address_start=sp,
-        extract=dbg.mem[sp, 8 * 8],
+        extract=dbg.memory[sp, 8 * 8],
         word_size=8,
         maps=dbg.maps,
         integer_mode=True,
@@ -52,8 +54,8 @@ def draw_stack(dbg: Debugger) -> None:
 
 
 # ── pretty printer ────────────────────────────────────────────────────
-def draw_context(dbg: Debugger) -> None:
-    """Draw the context of the debugger."""
+def draw_context(dbg: InternalDebugger) -> None:
+    """Draw the context of the devugger."""
     # Clear screen + home cursor
     print("\x1b[2J\x1b[H", end="")
 
@@ -63,14 +65,14 @@ def draw_context(dbg: Debugger) -> None:
     draw_backtrace(dbg)
 
 
-def start_tui(dbg: Debugger) -> None:
+def start_tui(dbg: InternalDebugger) -> None:
     """Tiny TUI for libdebug."""
-    while True:
-        draw_context(dbg)
-        try:
-            line = input("libdebug> ").strip()
-        except (EOFError, KeyboardInterrupt):
-            break
-
-        if line in ("q", "quit", "exit"):
-            break
+    dbg.is_in_tui = True
+    draw_context(dbg)
+    try:
+        # Start an IPython shell with the context of the user script
+        # TODO: what if libdebug is not run in the main module?
+        g = sys.modules["__main__"].__dict__
+        embed(user_ns=g, banner1="", banner2="", exit_msg="")
+    except (EOFError, KeyboardInterrupt):
+        return
