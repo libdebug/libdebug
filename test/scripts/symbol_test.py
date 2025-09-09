d6138208dde7c86531cf8c63147ae3c50b1057f7
io-no <gabriele18.06.98@gmail.com>
2025-09-09 02:31:40 +0200

    test: added the first tests on plt symbols

--- test/scripts/symbol_test.py
+++ test/scripts/symbol_test.py
@@ -4,8 +4,8 @@
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
-from unittest import TestCase
-from utils.binary_utils import RESOLVE_EXE
+from unittest import TestCase, skipUnless
+from utils.binary_utils import RESOLVE_EXE, PLATFORM
 
 from libdebug.data.symbol_list import SymbolList
 from libdebug.data.symbol import Symbol
@@ -37,6 +37,180 @@ class SymbolTest(TestCase):
         d.symbols["random_function"][0:1]
         d.symbols["random_function"][:]
         d.symbols["random_function"][-1]
+
+        d.kill()
+        d.terminate()
+
+    @skipUnless(PLATFORM == "i386", "Requires i386")
+    def test_plt_symbols_pie1(self):
+        d = debugger(RESOLVE_EXE("telescope_test"))
+
+        d.run()
+
+        symbols = d.symbols
+
+        libc_start_main_plt = symbols.filter("__libc_start_main@plt")
+        printf_plt = symbols.filter("printf@plt")
+        getchar_plt = symbols.filter("getchar@plt")
+        stack_chk_fail_plt = symbols.filter("__stack_chk_fail@plt")
+        puts_plt = symbols.filter("puts@plt")
+
+        self.assertEqual(len(libc_start_main_plt), 1)
+        self.assertEqual(len(printf_plt), 1)
+        self.assertEqual(len(getchar_plt), 1)
+        self.assertEqual(len(stack_chk_fail_plt), 1)
+        self.assertEqual(len(puts_plt), 1)
+
+        # Check the offset of the symbols
+        self.assertEqual(libc_start_main_plt[0].start, 0x1040)
+        self.assertEqual(printf_plt[0].start, 0x1050)
+        self.assertEqual(getchar_plt[0].start, 0x1060)
+        self.assertEqual(stack_chk_fail_plt[0].start, 0x1070)
+        self.assertEqual(puts_plt[0].start, 0x1080)
+
+        # No other plt symbols should be present
+        other_plt_symbols = [s for s in symbols if s.name.endswith("@plt") and s.name not in [
+            "__libc_start_main@plt",
+            "printf@plt",
+            "getchar@plt",
+            "__stack_chk_fail@plt",
+            "puts@plt"
+        ]]
+        self.assertEqual(len(other_plt_symbols), 0)
+
+        d.kill()
+        d.terminate()
+
+    @skipUnless(PLATFORM == "amd64", "Requires amd64")
+    def test_plt_symbols_pie2(self):
+        d = debugger(RESOLVE_EXE("telescope_test"))
+
+        d.run()
+
+        symbols = d.symbols
+
+        puts_plt = symbols.filter("puts@plt")
+        stack_chk_fail_plt = symbols.filter("__stack_chk_fail@plt")
+        printf_plt = symbols.filter("printf@plt")
+        getchar_plt = symbols.filter("getchar@plt")
+
+        self.assertEqual(len(puts_plt), 1)
+        self.assertEqual(len(stack_chk_fail_plt), 1)
+        self.assertEqual(len(printf_plt), 1)
+        self.assertEqual(len(getchar_plt), 1)
+
+        # Check the offset of the symbols
+        self.assertEqual(puts_plt[0].start, 0x1080)
+        self.assertEqual(stack_chk_fail_plt[0].start, 0x1090)
+        self.assertEqual(printf_plt[0].start, 0x10a0)
+        self.assertEqual(getchar_plt[0].start, 0x10b0)
+
+        # No other plt symbols should be present
+        other_plt_symbols = [s for s in symbols if s.name.endswith("@plt") and s.name not in [
+            "puts@plt",
+            "__stack_chk_fail@plt",
+            "printf@plt",
+            "getchar@plt"
+        ]]
+        self.assertEqual(len(other_plt_symbols), 0)
+
+        d.kill()
+        d.terminate()
+
+    @skipUnless(PLATFORM == "i386", "Requires i386")
+    def test_plt_symbols_no_pie1(self):
+        d = debugger(RESOLVE_EXE("basic_test"))
+
+        d.run()
+
+        symbols = d.symbols
+
+        libc_start_main_plt = symbols.filter("__libc_start_main@plt")
+        puts_plt = symbols.filter("puts@plt")
+
+        self.assertEqual(len(libc_start_main_plt), 1)
+        self.assertEqual(len(puts_plt), 1)
+
+        # Check the address of the symbols
+        self.assertEqual(libc_start_main_plt[0].start, 0x8049030)
+        self.assertEqual(puts_plt[0].start, 0x8049040)
+
+        # No other plt symbols should be present
+        other_plt_symbols = [s for s in symbols if s.name.endswith("@plt") and s.name not in [
+            "__libc_start_main@plt",
+            "puts@plt"
+        ]]
+        self.assertEqual(len(other_plt_symbols), 0)
+
+        d.kill()
+        d.terminate()
+
+    @skipUnless(PLATFORM == "amd64", "Requires amd64")
+    def test_plt_symbols_no_pie2(self):
+        d = debugger(RESOLVE_EXE("basic_test"))
+
+        d.run()
+
+        symbols = d.symbols
+
+        puts_plt = symbols.filter("puts@plt")
+
+        self.assertEqual(len(puts_plt), 1)
+
+        # Check the address of the symbols
+        self.assertEqual(puts_plt[0].start, 0x401030)
+
+        # No other plt symbols should be present
+        other_plt_symbols = [s for s in symbols if s.name.endswith("@plt") and s.name not in [
+            "puts@plt"
+        ]]
+        self.assertEqual(len(other_plt_symbols), 0)
+
+        d.kill()
+        d.terminate()
+
+    @skipUnless(PLATFORM == "i386", "Requires i386")
+    def test_plt_symbols_no_pie3(self):
+        d = debugger(RESOLVE_EXE("backtrace_test"))
+
+        d.run()
+
+        symbols = d.symbols
+
+        libc_start_main_plt = symbols.filter("__libc_start_main@plt")
+        printf_plt = symbols.filter("printf@plt")
+
+        self.assertEqual(len(libc_start_main_plt), 1)
+        self.assertEqual(len(printf_plt), 1)
+
+        # Check the address of the symbols
+        self.assertEqual(libc_start_main_plt[0].start, 0x8049030)
+        self.assertEqual(printf_plt[0].start, 0x8049040)        
+
+    @skipUnless(PLATFORM == "amd64", "Requires amd64")
+    def test_plt_symbols_no_pie4(self):
+        d = debugger(RESOLVE_EXE("breakpoint_test"))
+
+        d.run()
+
+        symbols = d.symbols
+
+        puts_plt = symbols.filter("puts@plt")
+        printf_plt = symbols.filter("printf@plt")
+
+        self.assertEqual(len(puts_plt), 1)
+        self.assertEqual(len(printf_plt), 1)
+
+        # Check the address of the symbols
+        self.assertEqual(puts_plt[0].start, 0x401030)
+        self.assertEqual(printf_plt[0].start, 0x401040)
+
+        # No other plt symbols should be present
+        other_plt_symbols = [s for s in symbols if s.name.endswith("@plt") and s.name not in [
+            "puts@plt",
+            "printf@plt"
+        ]]
+        self.assertEqual(len(other_plt_symbols), 0) 
 
         d.kill()
         d.terminate()
