c22c6164f49f6764bb854d18d7b7607db60b3e5f
Roberto Bertolini <robertobertolini37@gmail.com>
2026-05-19 16:30:32 -0500

    test: add unit tests for the new container feature

--- test/scripts/__init__.py
+++ test/scripts/__init__.py
@@ -8,6 +8,7 @@ from .backtrace_test import BacktraceTest
 from .breakpoint_test import BreakpointTest
 from .brute_test import BruteTest
 from .callback_test import CallbackTest
+from .container_utils_unit_test import ContainerUtilsUnitTest
 from .control_flow_test import ControlFlowTest
 from .corrupted_elf_test import CorruptedELFTest
 from .cursed_binaries_test import CursedBinariesTest
@@ -54,4 +55,4 @@ from .multiprocessing_test import MultiprocessingTest
 from .memory_leak_test import MemoryLeakTest
 
 
-__all__ = ["AliasTest", "AntidebugEscapingTest", "ArgumentListTest", "AtexitHandlerTest", "AttachDetachTest", "AutoWaitingTest", "BacktraceTest", "BreakpointTest", "BruteTest", "CallbackTest", "ControlFlowTest", "CorruptedELFTest", "CursedBinariesTest", "DebuggerMixinTest", "DeathTest", "DebuggerArgumentTest", "DeepDiveDivisionTest", "ELFUtilsUnitTest", "ExecTest", "FinishTest", "FloatingPointTest", "JumpoutTest", "JumpstartTest", "LargeBinarySymTest", "MemoryTest", "MemoryNoFastTest", "MultipleDebuggersTest", "NextTest", "NlinksTest", "PPrintSyscallsTest", "RegisterTest", "RunPipesTest", "SignalCatchTest", "SignalMultithreadTest", "SpeedTest", "SyscallHandleTest", "SyscallHijackTest", "TimeoutTest", "ThreadTest", "Vmwhere1Test", "WatchpointTest", "FindPointersTest", "SymbolTest", "SnapshotsTest", "MultiprocessingTest", "MemoryLeakTest"]
+__all__ = ["AliasTest", "AntidebugEscapingTest", "ArgumentListTest", "AtexitHandlerTest", "AttachDetachTest", "AutoWaitingTest", "BacktraceTest", "BreakpointTest", "BruteTest", "CallbackTest", "ControlFlowTest", "CorruptedELFTest", "CursedBinariesTest", "DebuggerMixinTest", "DeathTest", "DebuggerArgumentTest", "DeepDiveDivisionTest", "ELFUtilsUnitTest", "ExecTest", "FinishTest", "FloatingPointTest", "JumpoutTest", "JumpstartTest", "LargeBinarySymTest", "MemoryTest", "MemoryNoFastTest", "MultipleDebuggersTest", "NextTest", "NlinksTest", "PPrintSyscallsTest", "RegisterTest", "RunPipesTest", "SignalCatchTest", "SignalMultithreadTest", "SpeedTest", "SyscallHandleTest", "SyscallHijackTest", "TimeoutTest", "ThreadTest", "Vmwhere1Test", "WatchpointTest", "FindPointersTest", "SymbolTest", "SnapshotsTest", "MultiprocessingTest", "MemoryLeakTest", "ContainerUtilsUnitTest"]
